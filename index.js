73b61a1bf9a50012843b803bba8cc13737e88213
NitishKumarNanda <71919600+NitishKumarNanda@users.noreply.github.com>
2023-07-09 15:22:23 +0530

    try

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import Routes from './routes/route.js';
 const app = express();
 
 
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
